69b16b8cfbe607a60acd381e95cd513d35b83e59
Bayu Setiawan <sweetkobem@gmail.com>
2025-05-17 00:13:29 +0700

    Update dim_patient.sql

--- dbt/models/dim_patient/dim_patient.sql
+++ dbt/models/dim_patient/dim_patient.sql
@@ -1,18 +1,24 @@
 {{ config(
-    materialized='incremental',  
-    unique_key='dim_patient_key',
-    format='iceberg'
+    materialized='custom_incremental_iceberg'
 ) }}
 
 {% set execution_date = var('execution_date') %}
 {% set md5_column = var('md5_column') %}
 
-{% if is_incremental() %}
-  {% set max_end_date = run_query("SELECT CAST(MAX(start_date) AS STRING) FROM medalion.db.dim_patient WHERE is_active = TRUE").columns[0][0] %}
+{% set result = run_query("SHOW TABLES in "~ target.schema ~" LIKE '"~ this.name ~"'") %}
+{% if result and result.rows %}
+    {% set table_exists = result.rows[0]['tableName'] %}
+{% else %}
+    {% set table_exists = none %}
+{% endif %}
 
+{% if table_exists %}
+  {% set max_end_date = run_query("SELECT CAST(MAX(start_date) AS STRING) FROM "~ this ~" WHERE is_active = TRUE").columns[0][0] %}
 {% else %}
   {% set max_end_date = None %}
 {% endif %}
+
+{% do log("max_end_date:"~max_end_date, info=True) %}
 
 -- Check if max_end_date NULL or execution_date > max_end_date insert or update
 {% if max_end_date is none or execution_date > max_end_date %}
@@ -39,11 +45,13 @@
         TRUE AS is_active
 
     FROM medalion.db.slv_patients
-    {% if is_incremental() %}
-    WHERE {{ md5_column }} NOT IN (SELECT {{ md5_column }} FROM {{ this }} WHERE is_active = TRUE)  -- Avoid duplicates in incremental loads
+    {% if max_end_date %}
+      WHERE {{ md5_column }} NOT IN (SELECT {{ md5_column }} FROM {{ this }} WHERE is_active = TRUE)  -- Avoid duplicates in incremental loads
     {% endif %}
 
-  ), old_data AS (
+  )
+  {% if max_end_date %}
+  , old_data AS (
     SELECT
       dim_patient_key,
       patient_id,
@@ -69,10 +77,11 @@
     AND end_date IS NULL
 
   )
+  {% endif %}
   SELECT * FROM new_data
-  {% if is_incremental() %}
-  UNION ALL
-  SELECT * FROM old_data
+  {% if max_end_date %}
+    UNION ALL
+    SELECT * FROM old_data
   {% endif %}
 
 {% else %}
